bde811ea84fc016150a62a73a5b9966bb0114f0f
Usama Ashraf <usama.ashraf.khan@gmail.com>
2022-10-26 22:30:25 +0500

    Changes in Logic

--- pawn_simulator.rb
+++ pawn_simulator.rb
@@ -10,21 +10,25 @@ class PawnSimulator
     while @live
       print '> '
       input = gets.chomp
-      inputs = input.split(' ')
+      inputs = input.gsub(' ', ',').split(',')
       command = inputs.shift
       arguments = inputs
 
       case command
         when 'PLACE'
-          place(x: arguments[0], y: arguments[1], face: arguments[2], color: arguments[3])
+          @first_move, @invalid_move = false
+          place(x: arguments[0].to_i, y: arguments[1].to_i, face: arguments[2], color: arguments[3])
         when 'MOVE'
-          move(step: arguments[0])
+          is_valid_move?(arguments[0])
+          move(step: arguments[0] ? arguments[0].to_i : 1) if !@invalid_move
         when 'RIGHT'
           change_direction(direction: "RIGHT")
         when 'LEFT'
           change_direction(direction: "LEFT")
         when 'REPORT'
           report
+        when 'EXIT'
+          exit
       end
     end
   end
@@ -38,22 +42,33 @@ class PawnSimulator
   end
 
   def change_direction(direction:)
-    return if @pawn.nil?
+    if @pawn.nil?
+      puts 'First command should be of PLACE'
+      return
+    end
 
     if direction == 'RIGHT'
-      @pawn[:face] = directions[@pawn[:face]]
+      @pawn[:face] = directions[@pawn[:face].to_sym]
     else
-      @pawn[:face] = directions.key(@pawn[:face])
+      @pawn[:face] = directions.key(@pawn[:face]).to_s
     end
   end
 
   def move(step: )
-    return if @pawn.nil?
+    if @pawn.nil?
+      puts 'First command should be of PLACE'
+      return
+    end
+
     movement(face: @pawn[:face], step: step)
+    @first_move = true
   end
 
   def report
-    print 'NO PAWN' if @pawn.nil?
+    if @pawn.nil?
+      puts 'First command should be of PLACE'
+      return
+    end
 
     puts "#{@pawn[:location][0]}, #{@pawn[:location][1]}, #{@pawn[:face]}, #{@pawn[:color]}"
   end
@@ -72,25 +87,62 @@ class PawnSimulator
     }
   end
 
-  def movement(face:, step:)
-    if face == 'NORTH'
-      if @pawn[:location][0].to_i - step <= 0
-        @pawn[:location][0] = @pawn[:location][0].to_i - step
-      end
-    elsif face == 'SOUTH'
-      if @pawn[:location][0].to_i + step <= 7
-        @pawn[:location][0] = @pawn[:location][0].to_i + step
-      end
-    elsif face == 'EAST'
-      if @pawn[:location][1].to_i + step <= 7
-        @pawn[:location][1] = @pawn[:location][1].to_i + step
-      end
-    elsif face == 'WEST'
-      if @pawn[:location][1].to_i - step <= 0
-        @pawn[:location][1] = @pawn[:location][1].to_i + step
-      end
+  def is_valid_move?(move)
+    @invalid_move = false
+    if move.to_i >= 2 && @first_move
+      puts 'Invalid move!!! Move cannot be greater than 1 now.'
+      @invalid_move = true
+      return
+
+    elsif move.to_i >=3
+      puts 'Invalid move!!! Move cannot be greater than 2.'
+      @invalid_move = true
+      return
     end
   end
 
+  def north(location, step)
+    if location + step < 0 || location + step >= 8
+      puts 'This move will make pawn fall of the board'
+      return
+    end
+    @pawn[:location][1] = location + step
+  end
+
+  def south(location, step)
+    if location - step < 0 || location - step >= 8
+      puts 'This move will make pawn fall of the board'
+      return
+    end
+      @pawn[:location][1] = location - step
+  end
+
+  def east(location, step)
+    if location + step < 0 || location + step >= 8
+      puts 'This move will make pawn fall of the board'
+      return
+    end
+    @pawn[:location][0] = location + step
+  end
+
+  def west(location, step)
+    if location - step < 0 || location - step >= 8
+      puts 'This move will make pawn fall of the board'
+      return
+    end
+    @pawn[:location][0] = location - step
+  end
+
+  def movement(face:, step:)
+    if face == 'NORTH'
+      north(@pawn[:location][1], step)
+    elsif face == 'SOUTH'
+      south(@pawn[:location][1], step)
+    elsif face == 'EAST'
+      east(@pawn[:location][0], step)
+    elsif face == 'WEST'
+      west(@pawn[:location][0],step)
+    end
+  end
 end
 
